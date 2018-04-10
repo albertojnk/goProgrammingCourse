d45a0fc6b0911a81b0e22032beeab2afbb71375c
Alberto Janicke <albertojanicke@live.com>
2018-04-10 14:47:29 -0300

    ninja9ex4

---
+++ ninja9ex4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	inc := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			m.Lock()
+			v := inc
+			v++
+			inc = v
+			fmt.Println(inc)
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("end value: ", inc)
+}
